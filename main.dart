21b6d7bfeaa8600da312545a6cb8010887fec5a0
MinhAnh312 <56524674+MinhAnh312@users.noreply.github.com>
2021-02-04 09:48:12 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp(
+      items:List<ListItem>.generate(
+          1000, (i) => i%6==0
+          ? HeadingItem()
+          : MessageItem()
+      )
+  )
+  );
+}
+
+class MyApp extends StatelessWidget{
+  final List<ListItem> items;
+  MyApp({Key key, @required this.items}) : super(key: key);
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+        title: 'NoteBookApp',
+        theme: ThemeData(primaryColor: Colors.blue),
+        home: Scaffold(
+            body: ListView.builder(itemCount: items.length,
+                itemBuilder: (context, i)
+                {
+                  final item = items[i];
+                  return ListTile(title: item.buildTitle(context),
+                    subtitle: item.buildSubtitle(context),);
+                }
+            )
+        )
+    );
+  }
+}
+
+
+abstract class ListItem{
+  Widget buildTitle(BuildContext context);
+  Widget buildSubtitle(BuildContext context);
+
+}
+class HeadingItem implements ListItem {
+  //final String heading;
+
+  //HeadingItem(this.heading);
+
+  Widget buildTitle(BuildContext context) {
+    // return Text(
+    //   heading,
+    //   style: Theme.of(context).textTheme.headline5,
+    // );
+    return TextField(decoration: InputDecoration(
+        border: InputBorder.none,hintText: 'Enter your heading',
+        labelText: 'Heading', labelStyle: TextStyle(
+        color: Colors.blueGrey,
+        fontStyle: FontStyle.italic,fontSize: 15.0
+    )
+    )
+    );
+  }
+
+  Widget buildSubtitle(BuildContext context) => null;
+}
+class MessageItem implements ListItem {
+  //final String name;
+  //final String body;
+  //MessageItem(this.name, this.body);
+
+  Widget buildTitle(BuildContext context)
+  // => Text(name);
+  {
+    return TextField(decoration: InputDecoration(
+        border: InputBorder.none,hintText: 'Enter date that\'s you enter'
+    )
+    );
+  }
+  Widget buildSubtitle(BuildContext context)
+  //=> Text(body);
+  {
+    return TextField(decoration: InputDecoration(
+        border: InputBorder.none,hintText: 'Enter your note'
+    )
+    );
+  }
+}
